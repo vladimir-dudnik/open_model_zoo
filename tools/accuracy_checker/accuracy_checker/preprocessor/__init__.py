7594d0aab7775d67ef08c5d3f6bdf9b3f16dfb6d
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-07-06 21:21:44 +0300

    pvd2

--- tools/accuracy_checker/accuracy_checker/preprocessor/__init__.py
+++ tools/accuracy_checker/accuracy_checker/preprocessor/__init__.py
@@ -1,12 +1,14 @@
 """
+<<<<<<< 0eed3e8eb790d44c33e4e4a689d21cf5c7bf28f9
 Copyright (c) 2018-2020 Intel Corporation
 
+=======
+Copyright (c) 2019 Intel Corporation
+>>>>>>> pvd2
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
       http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -51,6 +53,7 @@ from .geometric_transformations import (
 )
 from .crop import (
     Crop, CropRect, ExtendAroundRect, Crop3D, TransformedCropWithAutoScale, CandidateCrop, CropOrPad, CropWithPadSize
+    CandidateCrop
 )
 from .resize import Resize, AutoResize
 from .nlp_preprocessors import DecodeByVocabulary, PadWithEOS

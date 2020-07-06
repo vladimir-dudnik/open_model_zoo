4501c0fc946dbb8b5eb6c13826cd383cef3ab2bc
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-07-06 21:21:44 +0300

    pvd2

--- tools/accuracy_checker/accuracy_checker/adapters/__init__.py
+++ tools/accuracy_checker/accuracy_checker/adapters/__init__.py
@@ -1,12 +1,9 @@
 """
 Copyright (c) 2019-2020 Intel Corporation
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
       http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -52,7 +49,10 @@ from .detection import (
     FaceDetectionAdapter,
     FaceDetectionRefinementAdapter
 )
-from .detection_person_vehicle import PersonVehicleDetectionAdapter
+from .detection_person_vehicle import (
+    PersonVehicleDetectionAdapter,
+    PersonVehicleDetectionRefinementAdapter
+)
 from .ssd import SSDAdapter, PyTorchSSDDecoder, FacePersonAdapter, SSDAdapterMxNet, SSDONNXAdapter
 from .retinaface import RetinaFaceAdapter
 from .yolo import TinyYOLOv1Adapter, YoloV2Adapter, YoloV3Adapter
@@ -98,6 +98,7 @@ __all__ = [
     'FaceDetectionAdapter',
     'FaceDetectionRefinementAdapter',
     'PersonVehicleDetectionAdapter',
+    'PersonVehicleDetectionRefinementAdapter',
 
     'SegmentationAdapter',
     'BrainTumorSegmentationAdapter',

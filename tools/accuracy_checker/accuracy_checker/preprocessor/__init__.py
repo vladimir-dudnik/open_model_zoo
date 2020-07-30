34c2fc69c6fc0814b7aa256f62314e603174e951
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-07-30 16:24:16 +0300

    Update __init__.py

--- tools/accuracy_checker/accuracy_checker/preprocessor/__init__.py
+++ tools/accuracy_checker/accuracy_checker/preprocessor/__init__.py
@@ -13,7 +13,6 @@ limitations under the License.
 
 from .preprocessing_executor import PreprocessingExecutor
 from .preprocessor import Preprocessor
-from .audio_preprocessing import ResampleAudio, ClipAudio, NormalizeAudio
 from .color_space_conversion import (
     BgrToRgb, RgbToBgr,
     BgrToGray, RgbToGray,
@@ -23,21 +22,29 @@ from .color_space_conversion import (
     BGRtoNV12Converter, RGBtoNV12Converter,
     NV12toBGRConverter, NV12toRGBConverter
 )
+from .audio_preprocessing import (
+    ResampleAudio,
+    ClipAudio,
+    NormalizeAudio,
+    HanningWindow,
+    AudioSpectrogram,
+    TriangleFiltering,
+    DCT,
+    ClipCepstrum
+)
+
 from .normalization import Normalize, Normalize3d
 from .geometric_transformations import (
     GeometricOperationMetadata,
     Flip,
-    Crop,
-    CropRect,
-    ExtendAroundRect,
     PointAligner,
     Tiling,
-    Crop3D,
-    TransformedCropWithAutoScale,
     ImagePyramid,
     FaceDetectionImagePyramid,
-    WarpAffine,
-    CandidateCrop
+    WarpAffine
+)
+from .crop import (
+    Crop, CropRect, ExtendAroundRect, Crop3D, TransformedCropWithAutoScale, CandidateCrop, CropOrPad
 )
 from .resize import Resize, AutoResize
 from .nlp_preprocessors import DecodeByVocabulary, PadWithEOS
@@ -54,24 +61,22 @@ __all__ = [
     'ResampleAudio',
     'ClipAudio',
     'NormalizeAudio',
+    'HanningWindow',
+    'AudioSpectrogram',
+    'TriangleFiltering',
+    'DCT',
+    'ClipCepstrum',
 
     'Resize',
     'Resize3D',
     'AutoResize',
     'Flip',
-    'Crop',
-    'CropRect',
-    'ExtendAroundRect',
     'PointAligner',
     'Tiling',
-    'Crop3D',
     'CropBraTS',
-    'TransformedCropWithAutoScale',
     'ImagePyramid',
     'FaceDetectionImagePyramid',
     'WarpAffine',
-    'CandidateCrop',
-
     'BgrToGray',
     'BgrToRgb',
     'RgbToGray',
@@ -79,6 +84,11 @@ __all__ = [
     'BGR2YUVConverter',
     'TfConvertImageDType',
     'SelectInputChannel',
+    'CropOrPad',
+    'Crop',
+    'CandidateCrop',
+    'CropRect',
+    'Crop3D',
 
     'Normalize3d',
     'Normalize',
